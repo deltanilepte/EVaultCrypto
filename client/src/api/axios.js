6bd79cc68f22a2bfcadbf2f57d2ad715557117f7
b3ingsahil007 <chhipasahil163@gmail.com>
2026-01-02 13:53:24 +0530

    Base Url Error Solved

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'e-vault-crypto-backend.vercel.app/api',
+    baseURL: 'https://e-vault-crypto-backend.vercel.app/api',
     headers: {
         'Content-Type': 'application/json',
     },
