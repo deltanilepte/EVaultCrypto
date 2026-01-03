7f91f5cd26b16cce9ee2732fcd7a6278f32469a3
b3ingsahil007 <chhipasahil163@gmail.com>
2026-01-03 19:19:51 +0530

    Base URL Set

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://e-vault-crypto-backend.vercel.app//api',
     headers: {
         'Content-Type': 'application/json',
     },
