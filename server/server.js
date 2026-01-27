580d6be269a1b8cf44770918aca05fed144ac2b3
b3ingsahil007 <chhipasahil163@gmail.com>
2026-01-27 16:15:12 +0530

    Fix CORS issue by removing trailing slash from origin

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ dotenv.config();
 
 // CORS
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://e-vault-crypto.vercel.app', 'https://e-vault-crypto-backend.vercel.app', 'https://e-vault-crypto-backend.vercel.app/api', 'http://localhost:5000', 'https://e-vault-crypto-frontend.vercel.app/', 'https://e-vault-crypto-backend-five.vercel.app/api'], // Allow both 5173 and 5174
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://e-vault-crypto.vercel.app', 'https://e-vault-crypto-backend.vercel.app', 'https://e-vault-crypto-backend.vercel.app/api', 'http://localhost:5000', 'https://e-vault-crypto-frontend.vercel.app', 'https://e-vault-crypto-backend-five.vercel.app/api'], // Allow both 5173 and 5174
     methods: 'GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH, PROPFIND',
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
