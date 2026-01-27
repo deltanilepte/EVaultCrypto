1ecce5bda16f21e56659a597a4cb8efa75a04c6b
b3ingsahil007 <chhipasahil163@gmail.com>
2026-01-27 16:07:37 +0530

    Second Commit

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://e-vault-crypto-backend.vercel.app/api',
+    baseURL: import.meta.env.VITE_API_URL || 'https://e-vault-crypto-backend-five.vercel.app/api',
     headers: {
         'Content-Type': 'application/json',
     },
